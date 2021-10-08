989e9d18111d1fe4dffa64b2131ea13f4b18c574
Thomas Fan <thomasfan100@gmail.com>
2021-10-07 19:56:53 -0500

    progress

--- Advanced Exercises/opencv2_exercises.py
+++ Advanced Exercises/opencv2_exercises.py
@@ -38,16 +38,46 @@ def roll(rolls):
 def wario(img):
     #split image
     img2 = img[0:210]
-    img3 = img[210:]
+    img3 = img[210:400]
+    img4 = img[400:]
+    #hat
     
     img2 = np.where(img2 == [243,65,53],[255,247,0], img2)
+    #hair
     img2 = np.where(img2 == [127,128,0], [0,0,0], img2)
+    #skin
     img2 = np.where(img2 == [255,151,0],[205,151,1], img2)
-    img = np.concatenate((img2,img3),axis = 0)
-    #img3 = img[210:]
-    #reds = np.where(meanb > 250,255,25)
+    #background
+    img2 = np.where(img2 == [254,254,254],[40,40,40], img2)
+    #img3 = np.where(img3 == [254,254,254],[40,40,40], img3)
+    img4 = np.where(img4 == [254,254,254],[40,40,40], img4)
+    #overalls
+    img3r = img3[:,:,0]
+    img3g = img3[:,:,1]
+    img3b = img3[:,:,2]
 
-    return img
+    a = np.array(
+    [[1,2,3],
+    [4,5,6],
+    [7,8,3]])
+    r = a[:,0]
+    g = a[:,1]
+    b = a[:,2]
+    mask = np.where((r == 1) & (g == 2) & (b == 3), 1,0)
+    mask = np.dstack((mask,mask,mask))
+    a = np.where(mask,[4,5,6],a)
+    print(a)
+    
+    #img3 = np.where(img3 == [243,65,53],[160,32,239],img3)
+    #hand/skin
+    img3 = np.where(img3 == (255,151,0) , [255,255,255],img3)
+    #img3 = np.where(img3 == 254 ,[255,255,255],img3 )
+    #shirt
+    #img3 = np.where(img3 == [127,128,254],[255,247,0], img3)
+    
+    img = np.concatenate((img2,img3),axis = 0)
+    img = np.concatenate((img,img4), axis = 0 )
+    return img3
 
 if __name__ == "__main__":
     '''
@@ -55,12 +85,20 @@ if __name__ == "__main__":
     NUM_TEST = 1000000 #multiple number of wins to get winrate
     rolls = craps(NUM_TEST)
     '''
+    
     #Mario Bros
     warioo = mpimg.imread('./wario.png')
     img = mpimg.imread('./mario.jpg')
     img = wario(img)
-    
+    '''
     plt.imshow(img)
     plt.show()
     plt.imshow(warioo)
     plt.show()
+    '''
+    '''
+    #tomatoes
+    img = mpimg.imread('./tomatoes.jpg')
+    plt.imshow(img)
+    plt.show()
+    '''
