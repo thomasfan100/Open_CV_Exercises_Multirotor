189bf3ef977fd7762f0555b495d191d234c302b4
Thomas Fan <thomasfan100@gmail.com>
2021-10-08 13:34:12 -0500

    Completed Mario

--- Advanced Exercises/opencv2_exercises.py
+++ Advanced Exercises/opencv2_exercises.py
@@ -41,43 +41,41 @@ def wario(img):
     img3 = img[210:400]
     img4 = img[400:]
     #hat
-    
-    img2 = np.where(img2 == [243,65,53],[255,247,0], img2)
+    img2 = changeColor(img2,[243,65,53],[255,247,0])
     #hair
-    img2 = np.where(img2 == [127,128,0], [0,0,0], img2)
+    img2 = changeColor(img2,[127,128,0], [0,0,0])
     #skin
-    img2 = np.where(img2 == [255,151,0],[205,151,1], img2)
+    img2 = changeColor(img2,[255,151,0],[205,151,1])
     #background
     img2 = np.where(img2 == [254,254,254],[40,40,40], img2)
-    #img3 = np.where(img3 == [254,254,254],[40,40,40], img3)
+    img3 = changeColor(img3,[254,254,254],[40,40,40])
     img4 = np.where(img4 == [254,254,254],[40,40,40], img4)
     #overalls
-    img3r = img3[:,:,0]
-    img3g = img3[:,:,1]
-    img3b = img3[:,:,2]
-
-    a = np.array(
-    [[1,2,3],
-    [4,5,6],
-    [7,8,3]])
-    r = a[:,0]
-    g = a[:,1]
-    b = a[:,2]
-    mask = np.where((r == 1) & (g == 2) & (b == 3), 1,0)
-    mask = np.dstack((mask,mask,mask))
-    a = np.where(mask,[4,5,6],a)
-    print(a)
-    
-    #img3 = np.where(img3 == [243,65,53],[160,32,239],img3)
+    img3 = changeColor(img3,[243,65,53],[160,32,239])
     #hand/skin
-    img3 = np.where(img3 == (255,151,0) , [255,255,255],img3)
-    #img3 = np.where(img3 == 254 ,[255,255,255],img3 )
+    img3 = changeColor(img3,[255,151,0] , [255,255,255])
     #shirt
-    #img3 = np.where(img3 == [127,128,254],[255,247,0], img3)
-    
+    img3 = changeColor(img3,[127,128,0],[255,247,0])
+
     img = np.concatenate((img2,img3),axis = 0)
     img = np.concatenate((img,img4), axis = 0 )
-    return img3
+    img = img[::2]
+    return img
+
+def changeColor(img,color, newcolor):
+    '''
+    Helper function for mario program. Changes the color by splitting into three channels. 
+    Returns an image with colors converted.
+    '''
+    imgr = img[:,:,0]
+    imgg = img[:,:,1]
+    imgb = img[:,:,2]
+    #210 235 250= 235
+    #mask = np.where((abs(imgr - color[0]) < 100) & (abs(imgg - color[1]) < 100) & (abs(imgb - color[2]) < 100), 1,0)
+    mask = np.where((imgr == color[0]) & (imgg == color[1]) & (imgb == color[2]), 1,0)
+    mask = np.dstack((mask,mask,mask))
+    img = np.where(mask, newcolor, img)
+    return img
 
 if __name__ == "__main__":
     '''
@@ -85,20 +83,20 @@ if __name__ == "__main__":
     NUM_TEST = 1000000 #multiple number of wins to get winrate
     rolls = craps(NUM_TEST)
     '''
-    
+    '''
     #Mario Bros
-    warioo = mpimg.imread('./wario.png')
+    wario_solution = mpimg.imread('./wario.png')
     img = mpimg.imread('./mario.jpg')
     img = wario(img)
-    '''
+    
     plt.imshow(img)
     plt.show()
-    plt.imshow(warioo)
+    plt.imshow(wario_solution)
     plt.show()
     '''
-    '''
+    
     #tomatoes
     img = mpimg.imread('./tomatoes.jpg')
     plt.imshow(img)
     plt.show()
-    '''
+    
